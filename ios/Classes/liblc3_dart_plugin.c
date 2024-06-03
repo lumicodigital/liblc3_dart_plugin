f6be5205641753863b2e1576f1167efea4833e9f
grigoriyk <grigoriyk@lumico.io>
2024-06-03 16:25:49 +0100

    added all dependencies to ios side

--- ios/Classes/liblc3_dart_plugin.c
+++ ios/Classes/liblc3_dart_plugin.c
@@ -1,3 +1,17 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in ../{projectName}}.podspec for more information.
+
+#include "../../liblc3/src/attdet.c"
+#include "../../liblc3/src/bits.c"
+#include "../../liblc3/src/bwdet.c"
+#include "../../liblc3/src/energy.c"
+#include "../../liblc3/src/lc3.c"
+#include "../../liblc3/src/ltpf.c"
+#include "../../liblc3/src/mdct.c"
+#include "../../liblc3/src/plc.c"
+#include "../../liblc3/src/sns.c"
+#include "../../liblc3/src/spec.c"
+#include "../../liblc3/src/tables.c"
+#include "../../liblc3/src/tns.c"
+
 #include "../../src/liblc3_dart_plugin.c"
