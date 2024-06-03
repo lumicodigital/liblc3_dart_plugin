d9daee64d518a48a40a5fba7201a195f48f4e1fe
grigoriyk <grigoriyk@lumico.io>
2024-06-03 15:19:54 +0100

    Initialized flutter ffi plugin, added lc3 decoder srouces

---
+++ ios/Classes/liblc3_dart_plugin.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../{projectName}}.podspec for more information.
+#include "../../src/liblc3_dart_plugin.c"
--- src/decoder.h
+++ src/decoder.h
@@ -3,6 +3,23 @@
 
 #include <stdint.h>
 
+//#include <stdint.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//#if _WIN32
+//#include <windows.h>
+//#else
+//#include <pthread.h>
+//#include <unistd.h>
+//#endif
+//
+//#if _WIN32
+//#define FFI_PLUGIN_EXPORT __declspec(dllexport)
+//#else
+//#define FFI_PLUGIN_EXPORT
+//#endif
+
 typedef enum {
   LC3_DECODER_OK = 0,
   LC3_DECODER_NOT_INITIALIZED,
